0f7064396cfab25993efe84c305eabec28c3639f
Luís <lfpcouti@gmail.com>
2025-09-16 21:48:45 -0300

    Aula 06 - Atualização 2

--- Aulas/aula06/app.js
+++ Aulas/aula06/app.js
@@ -14,4 +14,24 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+const tarefas = [];
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+});
+
+app.post('/tarefas', (req, res) => {
+    const novaTarefa = { ...req.body, id: tarefas.length+1};
+    tarefas.push(novaTarefa);
+    res.status(201).json(novaTarefa);
+});
+
+app.get("/tarefas/:id", (req,res) => {
+    const { id } = req.params;
+    const tarefaEncontrada = tarefas.find((item) => item.id === parseInt(id));
+    if (tarefaEncontrada) 
+        return res.json(tarefaEncontrada);
+    res.status(404).json({mse: "Tarefa não encontrada"});
+});
+
 module.exports = app;
