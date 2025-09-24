caab2b1fd4210f96c0bd55425d5ff382d3400475
Luís <lfpcouti@gmail.com>
2025-09-23 21:38:33 -0300

    Finalizado

--- Aulas/aula06/app.js
+++ Aulas/aula06/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
+const tarefaRouter = require("./routes/tarefasRoute");
 
 const app = express();
 
@@ -12,26 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/tarefas", tarefaRouter);
 
-const tarefas = [];
 
-app.get('/tarefas', (req, res) => {
-    res.json(tarefas);
-});
-
-app.post('/tarefas', (req, res) => {
-    const novaTarefa = { ...req.body, id: tarefas.length+1};
-    tarefas.push(novaTarefa);
-    res.status(201).json(novaTarefa);
-});
-
-app.get("/tarefas/:id", (req,res) => {
-    const { id } = req.params;
-    const tarefaEncontrada = tarefas.find((item) => item.id === parseInt(id));
-    if (tarefaEncontrada) 
-        return res.json(tarefaEncontrada);
-    res.status(404).json({mse: "Tarefa não encontrada"});
-});
 
 module.exports = app;
