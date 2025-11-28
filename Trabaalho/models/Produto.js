f367395e3387b94b748c6a059c9536a922774e75
Luís_F. <lfpcouti@gmail.com>
2025-11-27 23:46:19 -0300

    Trabalho

--- Trabaalho/models/Produto.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const produtosSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  quantidade: { type: Number, required: true },
-  validade: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Produto", produtosSchema);
